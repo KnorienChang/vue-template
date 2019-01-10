cad2d2d5afb8a9fd6268bf1a14fcd84ecd087868
knorien <zhangjinwei0320@hotmail.com>
2019-01-10 18:18:55 +0800

    调整publicPath/baseUrl配置，打包资源可全部使用oss或者cdn，默认不开启

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 const CompressionPlugin = require('compression-webpack-plugin');
 const BrotliPlugin = require('brotli-webpack-plugin');
 module.exports = {
+  // 在vue配置中，去除并限制改写publicPath，而改用baseUrl
+  // baseUrl可以设置成相对路径，默认为/
+  // 也可以设置成cdn/oss的URI链接，但是必须以协议开头即http://或者https://
+  // baseUrl:
+  //   process.env.NODE_ENV === 'production' ? 'https://cdn.domain.com/' : '/',
   outputDir: process.env.OUTPUT_DIR,
   // 取消生产环境的sourceMap
   productionSourceMap: false,
